c834b507c9c572b8760db349ee2468b93745ca0a
TheHuy22 <139510944+TheHuy22@users.noreply.github.com>
2023-07-28 02:00:27 +0700

    code

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://media.istockphoto.com/id/499503432/vi/video/nh%E1%BA%ABn-c%C6%B0%E1%BB%9Bi.jpg?s=640x640&k=20&c=po9vBxSGkBKEZ01tS0Tb-IjU88u2569Ii-YYoBFrxyI=" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.jxh0f0o.mongodb.net',
+    DB_USER: 'thehuytran2210',
+    DB_PASS: 'nguyentranthehuy2210',
+    DB_DATABASE: 'ShoppingFashionDiamond',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '999999999999999', // in milliseconds
+    EMAIL_USER: 'Thehuytran2210@gmail.com', // gmail service
+    EMAIL_PASS: 'nguyentranthehuy2210'
+  };
+  module.exports = MyConstants;
